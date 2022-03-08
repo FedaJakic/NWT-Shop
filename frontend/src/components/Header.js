2f8bd69b5f495fc1c19c4eb60bd71f8babf01f62
FedaJakic <fedja.jakic@gmail.com>
2022-03-08 18:47:07 +0100

    added Link to home on header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,25 +1,30 @@
 import React from 'react'
-import {Navbar, Nav, Container} from 'react-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <header>
-      <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
-  <Container>
-      <Navbar.Brand>ProShop</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="ms-auto">
-        <Nav.Link>
-          <i className='fas fa-shopping-cart'></i>Cart
-        </Nav.Link>
-        <Nav.Link>
-          <i className='fas fa-user'></i>Sign In
-        </Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand>
+            <Link className='header-brand-name' to='/'>
+              NWT Shop
+            </Link>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <Nav.Link>
+                <i className='fas fa-shopping-cart'></i>Cart
+              </Nav.Link>
+              <Nav.Link>
+                <i className='fas fa-user'></i>Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
   )
 }
